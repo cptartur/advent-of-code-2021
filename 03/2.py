37ce69bade3a1dd2eaeeae03c1930e7b16074c0f
Artur Michalek <52135326+cptartur@users.noreply.github.com>
2021-12-03 14:17:26 +0100

    Working day 03 solution

--- 03/2.py
+++ 03/2.py
@@ -1,26 +1,26 @@
 from collections import Counter
+from types import FunctionType
 
 
-def remove_numbers(t: list, position: int) -> list:
+def rating(t: list, position: int, f: FunctionType) -> list:
     if (len(t)) == 1:
-        return t
+        return "".join(t[0])
     t1 = t.copy()
-    t1 = [list(map(lambda kv: kv[0] if kv[1] == '1' else None, enumerate(entry))) for entry in t]
-    c = Counter(x for i in t for x in set(i) if x is not None)
-    most_common = '1' if c[position] > len(t) - c[position] else '0'
+    t1 = [list(map(lambda kv: kv[0] if kv[1] == '1' else None,
+               enumerate(entry))) for entry in t]
+    c = Counter(x for i in t1 for x in set(i) if x is not None)
+    most_common = f(c, position, len(t))
     t = list(filter(lambda entry: entry[position] == most_common, t))
-    remove_numbers(t, position + 1)
-    
+    return rating(t, position + 1, f)
+
 
 c = Counter()
-with open('inx.txt', 'r') as f:
+with open('in.txt', 'r') as f:
     t = [i for i in list(map(list, f.read().splitlines()))]
-    # t = [list(map(lambda kv: kv[0] if kv[1] == '1' else None, enumerate(entry))) for entry in t]
-    # c = Counter(x for i in t for x in set(i) if x is not None)
 
 t1 = t.copy()
-t1 = remove_numbers(t.copy(), 0)
-print(t1)
-# gamma_rate = "".join('1' if v > len(t) - v else '0' for _, v in sorted(c.items()))
-# epsilon_rate = "".join('0' if v > len(t) - v else '1' for _, v in sorted(c.items()))
-# print(int(gamma_rate, 2) * int(epsilon_rate, 2))
+oxygen_rating = rating(t.copy(), 0, lambda c,
+                       position, n: '1' if c[position] >= n - c[position] else '0')
+co2_rating = rating(t.copy(), 0, lambda c,
+                    position, n: '1' if c[position] < n - c[position] else '0')
+print(int(oxygen_rating, 2) * int(co2_rating, 2))
