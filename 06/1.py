d30af2508da9c2f67d996b9fdd3f30a2accbcb9f
Artur Michalek <52135326+cptartur@users.noreply.github.com>
2021-12-06 11:42:53 +0100

    Add day 06 solution

---
+++ 06/1.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+with open('in.txt', 'r') as f:
+    t = [int(i) for i in f.read().split(',')]
+days = Counter()
+days.update(t)
+for i in range(80):
+    days_new = Counter()
+    days_new.update({i: days[i + 1] for i in range(7, -1, -1)})
+    days_new.update({8: days[0], 6: days[0]})
+    days = days_new
+print(sum(days.values()))
+
+# 374927
