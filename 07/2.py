39f60ae6be6033a64e48c8d34e700e5541f23552
Artur Michalek <52135326+cptartur@users.noreply.github.com>
2021-12-07 13:18:03 +0100

    Replace for loop with sum

--- 07/2.py
+++ 07/2.py
@@ -4,6 +4,6 @@ with open('in.txt', 'r') as f:
     t = [int(i) for i in f.read().split(',')]
 lowest = sum(i for value in t for i in range(value + 1))
 for v in range(min(t), max(t) + 1):
-    lowest = min(lowest, sum(
-        i for value in t for i in range(floor(fabs(v - value)) + 1)))
+    lowest = min(lowest, sum(sum(range(floor(fabs(v - value)) + 1))
+                 for value in t))
 print(lowest)
