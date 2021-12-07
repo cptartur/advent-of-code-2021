392feb99e775a6e8d120b1bd8b3b069354c8eb12
Artur Michalek <52135326+cptartur@users.noreply.github.com>
2021-12-07 13:36:08 +0100

    Replace summing over range with arithmetic progression sum

--- 07/2.py
+++ 07/2.py
@@ -1,9 +1,16 @@
+from io import open_code
 from math import fabs, floor
+from functools import reduce
+
+
+def progression_sum(n: int) -> int:
+    return (1 + n) / 2 * n
+
 
 with open('in.txt', 'r') as f:
     t = [int(i) for i in f.read().split(',')]
 lowest = sum(i for value in t for i in range(value + 1))
 for v in range(min(t), max(t) + 1):
-    lowest = min(lowest, sum(sum(range(floor(fabs(v - value)) + 1))
-                 for value in t))
+    lowest = min(lowest, sum(
+        map(progression_sum, (fabs(v - value) for value in t))))
 print(lowest)
